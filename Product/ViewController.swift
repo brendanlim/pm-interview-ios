e73eb04bfa8b9a7ae5edd85ceeb293a9b111e48f
ಠ_ಠ <_>
2014-06-06 14:00:07 -0700

    Initial Commit

---
+++ Product/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Product
+//
+//  Created by Brendan G. Lim on 6/6/14.
+//  Copyright (c) 2014 Brendan Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
